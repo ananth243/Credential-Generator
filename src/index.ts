d2532f17a1c18f3c154bba5c43458908cf1103f3
ananth243 <raghavananth02@gmail.com>
2022-03-12 12:37:26 +0530

    Done with password and JWT

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 import { program as secrecy } from "commander";
 import { randomBytes } from "crypto";
-import { createWriteStream, existsSync } from "fs";
+import { sign } from "jsonwebtoken";
+import { open, close, write, createReadStream } from "fs";
+import { join } from "path";
+import { EOL } from "os";
+import { glitch, rainbow } from "chalk-animation";
+import { prompt } from "inquirer";
+import * as chalk from "chalk";
 
 secrecy
   .name("secrecy")
@@ -8,25 +14,31 @@ secrecy
   .version("1.0.0");
 
 secrecy
-  .option("-u", "--user", "Generated a random username")
-  .option("-p, --password", "Generates random password of default length 8")
-  .option("-h", "-hash", "Generates a hash with random salt")
-  .option(
-    "-jwt",
-    "--jsonwebtoken",
-    "Generates a random jwt with a random secret"
-  );
+  .option("-u, --user")
+  .option("-p, --password <type>", "8")
+  .option("-h, --hash <type>", `${genSalt()}`)
+  .option("-jwt, --jsonwebtoken <type>", "secret");
 
-secrecy.parse(process.argv);
+secrecy.parse();
+
+async function genSalt() {
+  const random = await randomBytes(16).toString("hex");
+  return random;
+}
 
 const options = secrecy.opts();
 
+if (options.jsonwebtoken) {
+  generateJWT(options.jsonwebtoken);
+}
+
 if (options.password) {
-  generatePassword();
+  generatePassword(parseInt(options.password));
 }
 
 if (options.user) {
   // Generate Username
+  console.log("Generate Username");
   generateUsername();
 }
 
@@ -34,27 +46,67 @@ if (options.hash) {
   generateHash();
 }
 
-if (options.jwt) {
-  generateJWT();
+async function generateJWT(secret: string) {
+  // Read file if exists    console.log(payload);
+
+  let data = "";
+  const readStream = await createReadStream(join(__dirname, "payload.json"));
+  readStream.on("error", (err) => {
+    const animation = glitch("Check if a file payload.json exists");
+    setTimeout(() => {
+      animation.stop();
+      process.exit(1);
+    }, 4000);
+  });
+  readStream.on("data", (chunk) => {
+    data += chunk;
+  });
+  readStream.on("close", () => {
+    const payload = JSON.parse(data.toString());
+    sign(payload, secret, { algorithm: "HS256" }, (err, token) => {
+      if (err) throw Error("Some error occured");
+      console.log("🔑 Your JWT is: " + token);
+      // const animation = rainbow("🔑 Your JWT is: " + token);
+      // setTimeout(() => {
+      //   animation.stop();
+      // }, 1000);
+    });
+  });
 }
 
-async function generateJWT() {
-  console.log("Generate JWT");
-}
 async function generateHash() {
   console.log("Generate JWT");
 }
 async function generateUsername() {
   console.log("Generate Username");
 }
-async function generatePassword(len = 8) {
+async function generatePassword(len: number) {
   const password = await randomBytes(len).toString("hex");
-  if (existsSync("./password.txt")) {
-    // Append password on new line
-  } else {
-    const writeStream = createWriteStream("password.txt");
-    writeStream.write(password);
-    writeStream.end();
-  }
-  console.log("Generated Password: ", password);
+  let answer = await prompt([
+    {
+      type: "input",
+      name: "identifier",
+      message: "What word do you want to associate with this password?",
+      default() {
+        return "Date of Creation";
+      },
+    },
+  ]);
+  if (answer.identifier === "Date of Creation")
+    answer.identifier = new Date().toDateString();
+  open(join(__dirname, "../", "password.txt"), "a", 666, (e, id) => {
+    write(id, `${answer.identifier}: ${password}` + EOL, null, "utf-8", () => {
+      close(id, () => {
+        const animation = glitch("🔑 saved to txt file");
+        setTimeout(() => {
+          animation.stop();
+          console.log("🔑:", password);
+          const newAnimation = rainbow("Advised to clear terminal", 0.5);
+          setTimeout(() => {
+            newAnimation.stop();
+          }, 4000);
+        }, 4000);
+      });
+    });
+  });
 }
